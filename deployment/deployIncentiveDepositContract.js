27363f731656f85b84d7ed307ea82b4198087de0
invocamanman <jesus.ligero@yahoo.es>
2022-01-04 21:36:10 +0100

    update deployment script

--- deployment/deployIncentiveDepositContract.js
+++ deployment/deployIncentiveDepositContract.js
@@ -7,7 +7,7 @@ async function main() {
     // xDai Address
     const sbcTokenAddress = "0x722fc4DAABFEaff81b97894fC623f91814a1BF68";
     const sbcDepositContractAddress = "0x0B98057eA310F4d31F2a452B414647007d1645d9";
-    const validatorNum = 4;
+    const validatorNumDefault = 4;
 
     /*
         Deploy Dappnode Incentive deposit contract
@@ -19,19 +19,20 @@ async function main() {
     console.log('sbcDepositContract:', sbcDepositContractAddress);
 
     const IncentiveDepositContractFactory = await ethers.getContractFactory('IncentiveDepositContract')
-    incentiveDepositContract = await upgrades.deployProxy(IncentiveDepositContractFactory, [sbcTokenAddress, sbcDepositContractAddress, validatorNum])
+    incentiveDepositContract = await upgrades.deployProxy(IncentiveDepositContractFactory, [sbcTokenAddress, sbcDepositContractAddress, validatorNumDefault])
     await incentiveDepositContract.deployed();
 
     console.log('#######################\n');
     console.log('Dappnode Incentive contract deployed to:', incentiveDepositContract.address);
-
 
     console.log('\n#######################');
     console.log('#####    Checks    #####');
     console.log('#######################');
     console.log('sbcToken:', await incentiveDepositContract.sbcToken());
     console.log('sbcDepositContract:', await incentiveDepositContract.sbcDepositContract());
+    console.log('validatorNum:', await incentiveDepositContract.validatorNum());
     console.log('DEPOSIT_AMOUNT:', await incentiveDepositContract.DEPOSIT_AMOUNT());
+    console.log('INCENTIVE_DURATION:', await incentiveDepositContract.INCENTIVE_DURATION());
 }
 
 main().catch((e) => {
